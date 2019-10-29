cd85d10677246678a50db67484aef3d57b7f72ca
William <williamgeoffreycox@churchofjesuschrist.org>
2019-10-29 16:37:18 -0700

    update

--- config-example.js
+++ config-example.js
@@ -1,4 +1,4 @@
 module.exports = {
   settingsPath: '/Users/USERNAME_GOES_HERE/Library/Application Support/Code/User/settings.json',
-  keyboardPath: '/Users/USERNAME_GOES_HERE/Library/Application Support/Code/User/keybindings.json'
+  keybindingsPath: '/Users/USERNAME_GOES_HERE/Library/Application Support/Code/User/keybindings.json'
 }
