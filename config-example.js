bd8f2d4f9492c1d348c54769aba2f68cae19103e
William <williamgeoffreycox@churchofjesuschrist.org>
2019-10-29 16:19:54 -0700

    adding keybindings

---
+++ config-example.js
@@ -0,0 +1,4 @@
+module.exports = {
+  settingsPath: '/Users/USERNAME_GOES_HERE/Library/Application Support/Code/User/settings.json',
+  keyboardShortcuts: '/Users/USERNAME_GOES_HERE/Library/Application Support/Code/User/keybindings.json'
+}
